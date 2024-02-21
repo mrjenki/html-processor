a4533e90241062fcebbc6595136cce719ae172b4
mrjenki <jenki@cocc.vn>
2024-02-21 16:07:42 +0700

    1

--- httpclient.go
+++ httpclient.go
@@ -11,80 +11,80 @@ import (
 
 // CustomHTTPResponse represents the structure of the custom HTTP response.
 type CustomHTTPResponse struct {
-    FullHeader   http.Header `json:"full_header"`
-    RequestedURL string `json:"requested_url"`
-    ForwardedURL string `json:"forwarded_url"`
-    HTMLBody     string `json:"html_body"`
-    HTTPCode     int    `json:"http_code"`
+	FullHeader   http.Header `json:"full_header"`
+	RequestedURL string      `json:"requested_url"`
+	ForwardedURL string      `json:"forwarded_url"`
+	HTMLBody     string      `json:"html_body"`
+	HTTPCode     int         `json:"http_code"`
 }
 
 // FetchURLWithCustomResponse sends an HTTP GET request to the target URL with the specified headers
 // and returns a CustomHTTPResponse struct.
-func FetchURLWithCustomResponse(targetURL, hostHeader string, otherHeaders ...map[string]string) (CustomHTTPResponse, error) {
-    // Create an HTTP client with custom settings
-    tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // Disable SSL certificate verification
-        DialContext: (&net.Dialer{
-            Timeout:   30 * time.Second, // Connection timeout
-            KeepAlive: 10 * time.Second, // Keep-alive period
-        }).DialContext,
-        MaxIdleConns:          100000,
-        IdleConnTimeout:       2 * time.Second,
-        TLSHandshakeTimeout:   2 * time.Second,
-        ExpectContinueTimeout: 1 * time.Second,
-        MaxIdleConnsPerHost:   100000,
-    }
-    client := &http.Client{Transport: tr}
+func FetchURLWithCustomResponse(targetURL, hostHeader string, otherHeaders ...map[string][]string) (CustomHTTPResponse, error) {
+	// Create an HTTP client with custom settings
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // Disable SSL certificate verification
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second, // Connection timeout
+			KeepAlive: 10 * time.Second, // Keep-alive period
+		}).DialContext,
+		MaxIdleConns:          100000,
+		IdleConnTimeout:       2 * time.Second,
+		TLSHandshakeTimeout:   2 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		MaxIdleConnsPerHost:   100000,
+	}
+	client := &http.Client{Transport: tr}
+	// set request headers
 
-    // Create an HTTP request with the target URL
-    req, err := http.NewRequest("GET", targetURL, nil)
-    if err != nil {
-        return CustomHTTPResponse{}, err
-    }
+	// Create an HTTP request with the target URL
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		return CustomHTTPResponse{}, err
+	}
 
-    // Set the 'Host' header
-    req.Header.Set("Host", hostHeader)
-    if len(otherHeaders) > 0 {
-   // Add additional headers if provided
-    for k, v := range otherHeaders[0] {
-        req.Header.Set(k, v)
-    }
-}
+	// Set the 'Host' header
+	req.Header.Set("Host", hostHeader)
+	if len(otherHeaders) > 0 {
+		for k, v := range otherHeaders[0] {
+			req.Header[k] = v
+		}
+	}
 
-    // Send the HTTP request
-    resp, err := client.Do(req)
-    if err != nil {
-        return CustomHTTPResponse{}, err
-    }
-    defer resp.Body.Close()
+	// Send the HTTP request
+	resp, err := client.Do(req)
+	if err != nil {
+		return CustomHTTPResponse{}, err
+	}
+	defer resp.Body.Close()
 
-    // Read the response body
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return CustomHTTPResponse{}, err
-    }
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return CustomHTTPResponse{}, err
+	}
 
-    // Create a CustomHTTPResponse struct and populate its fields
-    // headers,err:=json.Marshal(resp.Header)
-    // if err != nil {
-    // }
-    customResponse := CustomHTTPResponse{
-        FullHeader:   resp.Header,
-        RequestedURL: targetURL,
-        ForwardedURL: resp.Request.URL.String(),
-        HTMLBody:     string(body),
-        HTTPCode:     resp.StatusCode,
-    }
-// try to close connection
-    resp.Body.Close()
-    return customResponse, nil
+	// Create a CustomHTTPResponse struct and populate its fields
+	// headers,err:=json.Marshal(resp.Header)
+	// if err != nil {
+	// }
+	customResponse := CustomHTTPResponse{
+		FullHeader:   resp.Header,
+		RequestedURL: targetURL,
+		ForwardedURL: resp.Request.URL.String(),
+		HTMLBody:     string(body),
+		HTTPCode:     resp.StatusCode,
+	}
+	// try to close connection
+	resp.Body.Close()
+	return customResponse, nil
 }
 
 // ToJSON returns a JSON representation of the CustomHTTPResponse struct.
-func (c CustomHTTPResponse) ToJSON() (string) {
-    jsonResponse, err := json.Marshal(c)
-    if err != nil {
-        return ""
-    }
-    return string(jsonResponse)
+func (c CustomHTTPResponse) ToJSON() string {
+	jsonResponse, err := json.Marshal(c)
+	if err != nil {
+		return ""
+	}
+	return string(jsonResponse)
 }
